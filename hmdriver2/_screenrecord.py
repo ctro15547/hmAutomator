5455d08b821c093ef76bb31f0cd7553648fd161f
zhm-mbp <2656117195@qq.com>
2025-06-06 23:02:12 +0800

    feat: 在RecordClient中添加分辨率调整功能，压缩视频帧以适应目标尺寸

--- hmdriver2/_screenrecord.py
+++ hmdriver2/_screenrecord.py
@@ -24,6 +24,8 @@ class RecordClient(HmClient):
         self.threads: typing.List[threading.Thread] = []
         self.stop_event = threading.Event()
 
+        self.target_width, self.target_height = self.d.display_size
+
     def __enter__(self):
         return self
 
@@ -96,18 +98,38 @@ class RecordClient(HmClient):
         """Write frames to video file."""
         cv2_instance = None
         img = None
+
+        target_width = int(self.target_width * 0.5)
+        target_height = int(self.target_height * 0.5)
+        quality = 30
         while not self.stop_event.is_set():
             try:
-                jpeg_image = self.jpeg_queue.get(timeout=0.1)
+                jpeg_image = self.jpeg_queue.get(timeout=0.05)
                 img = cv2.imdecode(np.frombuffer(jpeg_image, np.uint8), cv2.IMREAD_COLOR)
+
+                # === 新增：分辨率调整 ===
+                scaled_img = cv2.resize(
+                    img, 
+                    (target_width, target_height),  # 目标尺寸
+                    interpolation=cv2.INTER_AREA  # 推荐用于缩小图像
+                )
+
+                # === 继续压缩流程 ===
+                _, compressed_jpeg = cv2.imencode(
+                    '.jpg', 
+                    scaled_img,  # 使用缩放后的图像
+                    [int(cv2.IMWRITE_JPEG_QUALITY), quality]
+                )
+
+                compressed_bytes = compressed_jpeg.tobytes()
+                img = cv2.imdecode(np.frombuffer(compressed_bytes, np.uint8), cv2.IMREAD_COLOR)
             except queue.Empty:
                 pass
             if img is None or img.size == 0:
                 continue
             if cv2_instance is None:
-                height, width = img.shape[:2]
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                cv2_instance = cv2.VideoWriter(self.video_path, fourcc, 10, (width, height))
+                cv2_instance = cv2.VideoWriter(self.video_path, fourcc, 10, (target_width, target_height))
 
             cv2_instance.write(img)
 
